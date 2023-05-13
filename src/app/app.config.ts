cf642cc343da21a8fb2e5b5e098fbd8d59ab64d0
walle4561 <walle4561@gmail.com>
2023-05-13 16:39:08 +0800

    Http Get

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    provideHttpClient()
+  ],
 };
